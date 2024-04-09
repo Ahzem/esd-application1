3788ab6b9d7aa58bde3bcbaee9a541792b5fd203
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-04-09 11:31:49 +0530

    all updated

---
+++ src/StudentModel.java
@@ -0,0 +1,30 @@
+// Model
+public class StudentModel {
+    private String studentName;
+    private String indexNumber;
+    private int age;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String setStudentName) {
+        this.studentName = setStudentName;
+    }
+
+    public String getIndexNumber() {
+        return indexNumber;
+    }
+
+    public void setIndexNumber(String indexNumber) {
+        this.indexNumber = indexNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/StudentView.java
@@ -0,0 +1,9 @@
+// View
+public class StudentView {
+    public void printStudentDetails(String studentName, String indexNumber, int age) {
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Index number: " + indexNumber);
+        System.out.println("Age: " + age);
+    }
+}
